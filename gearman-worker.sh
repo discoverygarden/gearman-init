abfc4d6995171ee6712b065c11adc1833dbbefa8
Adam Vessey <adam-vessey@users.noreply.github.com>
2015-07-07 16:10:16 -0300

    Add comment.

--- gearman-worker.sh
+++ gearman-worker.sh
@@ -2,6 +2,7 @@
 
 . /opt/gearman-init/gearman-defaults.sh
 
+# Get a Bash array containing a list of all the job names.
 GEARMAN_FUNCTIONS=(${GEARMAN_FUNCTIONS[@]:-`$DRUSH --root=$DRUPAL_ROOT php-eval --format=string "return implode('$IFS', array_keys(module_invoke_all('islandora_job_register_jobs')));"`})
-cd $DRUPAL_ROOT
-$GEARMAN_BIN -v -h $GEARMAN_HOST -p $GEARMAN_PORT -w ${GEARMAN_FUNCTIONS[@]/#/-f } $DRUSH -u 1 islandora-job-router
+
+$GEARMAN_BIN -v -h $GEARMAN_HOST -p $GEARMAN_PORT -w ${GEARMAN_FUNCTIONS[@]/#/-f } $DRUSH --root=$DRUPAL_ROOT -u 1 islandora-job-router
