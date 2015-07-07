3b26d54535d1fd5f36ddea3ab34351ef0e455cbf
Adam Vessey <adam-vessey@users.noreply.github.com>
2015-07-07 16:22:54 -0300

    Fix parsing of command argument to gearman.
    Could potentially run into other trouble, if a job name ever contains
    hyphens or spaces.

--- gearman-worker.sh
+++ gearman-worker.sh
@@ -5,4 +5,4 @@
 # Get a Bash array containing a list of all the job names.
 GEARMAN_FUNCTIONS=(${GEARMAN_FUNCTIONS[@]:-`$DRUSH --root=$DRUPAL_ROOT php-eval --format=string "return implode('$IFS', array_keys(module_invoke_all('islandora_job_register_jobs')));"`})
 
-$GEARMAN_BIN -v -h $GEARMAN_HOST -p $GEARMAN_PORT -w ${GEARMAN_FUNCTIONS[@]/#/-f } $DRUSH --root=$DRUPAL_ROOT -u 1 islandora-job-router
+$GEARMAN_BIN -v -h $GEARMAN_HOST -p $GEARMAN_PORT -w ${GEARMAN_FUNCTIONS[@]/#/-f } -- $DRUSH --root=$DRUPAL_ROOT -u 1 islandora-job-router
