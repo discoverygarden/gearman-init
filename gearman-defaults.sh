6660c31c3efec3bee28c69137fbbdffccf0d2cb4
Adam Vessey <adam@discoverygarden.ca>
2016-04-06 10:35:47 -0300

    Fix variable gets.

--- gearman-defaults.sh
+++ gearman-defaults.sh
@@ -10,7 +10,8 @@
 DRUPAL_ROOT=${DRUPAL_ROOT:-/var/www/drupal7} 
 DRUSH=${DRUSH:-`which drush`}
 CPU_COUNT=${CPU_COUNT:-`find /sys/devices/system/cpu -maxdepth 1 -type d -regex '.*/cpu[0-9]+$' | wc -l`}
-GEARMAN_HOST=${GEARMAN_HOST:-`$DRUSH --root=$DRUPAL_ROOT variable-get islandora_job_server_host --exact 2> /dev/null || echo -n 'localhost'`}
-GEARMAN_PORT=${GEARMAN_PORT:-`$DRUSH --root=$DRUPAL_ROOT variable-get islandora_job_server_port --exact 2> /dev/null || echo -n '4730'`}
+GEARMAN_HOST=${GEARMAN_HOST:-`$DRUSH --root=$DRUPAL_ROOT variable-get islandora_job_server_host --exact --format=string 2> /dev/null || echo -n 'localhost'`}
+GEARMAN_PORT=${GEARMAN_PORT:-`$DRUSH --root=$DRUPAL_ROOT variable-get islandora_job_server_port --exact --format=string 2> /dev/null || echo -n '4730'`}
 GEARMAN_BIN=${GEARMAN_BIN:-`which gearman`}
 GEARMAN_USER=${GEARMAN_USER:-www-data}
+echo $GEARMAN_HOST
