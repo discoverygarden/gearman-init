10bbb6cdab8c69eb7a3f451f8c4a7aff46a7b0fa
qadan <daitken@discoverygarden.ca>
2017-10-06 16:39:57 -0300

    whoops

--- gearman-defaults.sh
+++ gearman-defaults.sh
@@ -17,4 +17,4 @@ GEARMAN_USER=${GEARMAN_USER:-www-data}
 # For multisites requiring per-site routing, a different router is provided
 # at https://github.com/discoverygarden/gearman-multisite-job-router
 ROUTER=${ROUTER:-"$DRUSH --root=$DRUPAL_ROOT -u 1 islandora-job-router"}
-SITE_URI_LIST=${SITE_URI_LIST:-`$drush --root=$DRUPAL_ROOT @sites status --interactive --field-labels=0 --fields=uri | sed -z 's/\n//g'`}
+SITE_URI_LIST=${SITE_URI_LIST:-`$DRUSH --root=$DRUPAL_ROOT @sites status --interactive --field-labels=0 --fields=uri | sed -z 's/\n//g'`}
